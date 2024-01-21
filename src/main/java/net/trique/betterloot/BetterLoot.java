5e70418a82adb19b6ab04f00fbd700e99530e79a
TriQue06 <baristanlik@gmail.com>
2024-01-21 15:26:30 +0300

    Better Loot 1.0.0

---
+++ src/main/java/net/trique/betterloot/BetterLoot.java
@@ -0,0 +1,15 @@
+package net.trique.betterloot;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BetterLoot implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("betterloot");
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Eren Yağlıdere üç de, wigu wigu dındıdı dın ıdnı dnı dnı");
+	}
+}
